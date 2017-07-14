6016af90180274b9721a73b912d00dc4529e77cd
Alexander M <xialexanderix@hotmail.com>
2017-07-13 22:30:18 -0500

    Create config.js

---
+++ src/assets/js/config.js
@@ -0,0 +1,49 @@
+/*
+    This code was rushed by xIAlexaderIx 
+    but made with <3 for the BABYMETAL community
+*/
+
+
+/* Github Config | BEGIN */
+
+var org = 'DeloreanSubs'; // The username or Org name that holds the repo
+
+var repo = 'tokyodome'; // Name of the repo
+
+var firstDeloreanBranch = 'rednight'; // Branch name of the first delorean
+
+var secondDeloreanBranch = 'blacknight'; // Branch name of the second delorean
+
+/* Github Config | END */
+
+
+/*
+    One for the money! Two for the Money! Three for the money!
+    Money! Money! Money! Money!
+*/
+
+
+/* Buy me a Coffee ( Help me stay awake during the making of the subs :P ) | BEGIN */
+
+var paypalMeCoffee = 'https://u.muxy.io/tip/xialexanderix'; // Link for the paypal link
+
+var bitcoinCoffee = '1HZxZp2z2DUFp4G3cfLY9DKZRr8MQzrkV2'; // Adress of the bitcoin wallet
+
+var qrBitcoinWallet = 'img/large/btc.svg'; // QR Code of the bitcoin wallet
+
+/* Buy me a Coffee | END */
+
+
+/*
+    Katte! Katte! Katte! Katte!
+    Choodai! Choodai! Choodai! Choodai!
+*/
+
+
+/* Bluray Links | BEGIN */
+
+var firstDeloreanBluray = 'https://goo.gl/2cz9xm'; // Store link for the first bluray
+
+var secondDeloreanBluray = 'https://goo.gl/2cz9xm'; // Store link for the second bluray
+
+/* Bluray Links | END */
