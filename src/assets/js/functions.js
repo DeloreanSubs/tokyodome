80ce58cbda4052b056e7b25b1977455a89e4fc65
Alexander M <xialexanderix@hotmail.com>
2017-07-13 22:28:08 -0500

    Update functions.js

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,43 +1,125 @@
+/*
+    This code was rushed by xIAlexaderIx 
+    but made with <3 for the BABYMETAL community
+    (Ain't Nobody Got Time For Commenting Dat Code)
+*/
+
 $(document).ready(function () {
     new WOW().init();
     $.material.init();
+    smoothBtnScroll();
     releaseStats();
     responsiveStats();
-    smoothBtnScroll();
+    hideYoWallet();
+    gimmeCoffe();
     cryptoPop();
     focusPocus();
 });
 
 function releaseStats() {
-    $.getJSON("https://api.github.com/repos/DeloreanSubs/tokyodome/releases/latest").done(function (release) {
-        var repo = release.assets[0];
-        var downloadCount = 0;
-        for (var i = 0; i < release.assets.length; i++) {
-            downloadCount += release.assets[i].download_count;
+    $.getJSON('https://api.github.com/repos' + '/' + org + '/' + repo + '/' + 'releases', function (github) {
+
+        var firstDelorean = $.grep(github, function (json) {
+            return json.target_commitish == firstDeloreanBranch;
+        });
+
+        var firstDeloreanLastUpdate;
+        try {
+            firstDeloreanLastUpdate = new Date(firstDelorean[0].assets[0].updated_at);
+            firstDeloreanLastUpdate = firstDeloreanLastUpdate.getDate() + '/' + firstDeloreanLastUpdate.getMonth() + '/' + firstDeloreanLastUpdate.getFullYear();
+        } catch (err) {
+            console.log('Error: ' + err.message);
         }
 
-        var engDownloadCount = release.assets[0].download_count;
-        var spaDownloadCount = release.assets[1].download_count;
+        var firstDeloreanENG;
+        var firstDeloreanES;
+        var firstDeloreanDL;
+        try {
+            firstDeloreanENG = firstDelorean[0].assets[0].browser_download_url;
+            firstDeloreanES = firstDelorean[0].assets[1].browser_download_url;
+            firstDeloreanDL = firstDelorean[0].assets[2].browser_download_url;
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
 
-        var engDownloadLink = release.assets[0].browser_download_url;
-        var spaDownloadLink = release.assets[1].browser_download_url;
-        var DownloadLink = release.assets[2].browser_download_url;
+        var firstDeloreanCount = 0;
+        var firstDeloreanCountENG;
+        var firstDeloreanCountES;
+        var firstDeloreanCountMIX;
+        try {
+            for (var a = 0; a < firstDelorean.length; a++) {
+                for (var b = 0; b < firstDelorean[a].assets.length; b++) {
+                    firstDeloreanCount += firstDelorean[a].assets[b].download_count;
+                }
+                firstDeloreanCountENG = firstDelorean[a].assets[0].download_count;
+                firstDeloreanCountES = firstDelorean[a].assets[1].download_count;
+                firstDeloreanCountMIX = firstDelorean[a].assets[2].download_count;
+            }
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
 
-        var jsonDate = new Date(release.assets[0].updated_at);
-        var releaseDate = jsonDate.getDate() + '/' + jsonDate.getMonth() + '/' + jsonDate.getFullYear();
+        $(".td-rn-download-eng").attr("href", firstDeloreanENG);
+        $(".td-rn-download-spa").attr("href", firstDeloreanES);
+        $(".td-rn-download").attr("href", firstDeloreanDL);
+        $(".td-rn-git").attr("href", 'https://github.com/' + '/' + org + '/' + repo + '/' + 'tree' + '/' + firstDeloreanBranch);
 
-        var totalDownloads = downloadCount.toLocaleString();
-        var engDownloads = engDownloadCount.toLocaleString();
-        var spaDownloads = spaDownloadCount.toLocaleString();
+        $('.firstDelorean-eng').text(firstDeloreanCountENG + firstDeloreanCountMIX);
+        $(".firstDelorean-es").text(firstDeloreanCountES + firstDeloreanCountMIX);
+        $(".firstDelorean-total").text(firstDeloreanCount);
+        $(".firstDelorean-date").text(firstDeloreanLastUpdate);
 
 
-        $(".td-rn-download").attr("href", DownloadLink);
-        $(".td-rn-download-spa").attr("href", spaDownloadLink);
-        $(".td-rn-download-eng").attr("href", engDownloadLink);
-        $(".release-eng").text(engDownloads);
-        $(".release-spa").text(spaDownloads);
-        $(".release-total").text(totalDownloads);
-        $(".release-date").text(releaseDate);
+        var secondDelorean = $.grep(github, function (json) {
+            return json.target_commitish == secondDeloreanBranch;
+        });
+
+        var secondDeloreanLastUpdate;
+        try {
+            secondDeloreanLastUpdate = new Date(secondDelorean[0].assets[0].updated_at);
+            secondDeloreanLastUpdate = secondDeloreanLastUpdate.getDate() + '/' + secondDeloreanLastUpdate.getMonth() + '/' + secondDeloreanLastUpdate.getFullYear();
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
+
+        var secondDeloreanENG;
+        var secondDeloreanES;
+        var secondDeloreanDL;
+        try {
+            secondDeloreanENG = secondDelorean[0].assets[0].browser_download_url;
+            secondDeloreanES = secondDelorean[0].assets[1].browser_download_url;
+            secondDeloreanDL = secondDelorean[0].assets[2].browser_download_url;
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
+
+        var secondDeloreanCount = 0;
+        var secondDeloreanCountENG;
+        var secondDeloreanCountES;
+        var secondDeloreanCountMIX;
+        try {
+            for (var c = 0; c < secondDelorean.length; c++) {
+                for (var d = 0; d < secondDelorean[c].assets.length; d++) {
+                    secondDeloreanCount += secondDelorean[c].assets[d].download_count;
+                }
+                secondDeloreanCountENG = secondDelorean[c].assets[0].download_count;
+                secondDeloreanCountES = secondDelorean[c].assets[1].download_count;
+                secondDeloreanCountMIX = secondDelorean[c].assets[2].download_count;
+            }
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
+
+        $(".td-bn-download-eng").attr("href", secondDeloreanENG);
+        $(".td-bn-download-spa").attr("href", secondDeloreanES);
+        $(".td-bn-download").attr("href", secondDeloreanDL);
+        $(".td-bn-git").attr("href", 'https://github.com/' + '/' + org + '/' + repo + '/' + 'tree' + '/' + secondDeloreanBranch);
+
+        $('.secondDelorean-eng').text(secondDeloreanCountENG + secondDeloreanCountMIX);
+        $(".secondDelorean-es").text(secondDeloreanCountES + secondDeloreanCountMIX);
+        $(".secondDelorean-total").text(secondDeloreanCount);
+        $(".secondDelorean-date").text(secondDeloreanLastUpdate);
+
     });
 }
 
@@ -78,6 +160,17 @@ function smoothBtnScroll() {
     });
 }
 
+function hideYoWallet() {
+    $(".td-rn-br").attr("href", firstDeloreanBluray);
+    $(".td-bn-br").attr("href", secondDeloreanBluray);
+}
+
+function gimmeCoffe() {
+    $(".pp").attr("href", paypalMeCoffee);
+    $(".qr").attr("src", qrBitcoinWallet);
+    $('.wallet').text(bitcoinCoffee);
+}
+
 function cryptoPop() {
     $('[rel="popover"]').popover({
         container: 'body',
@@ -97,7 +190,7 @@ function cryptoPop() {
 function focusPocus() {
     document.addEventListener('scroll', function () {
         $(':focus').blur();
-    }, true /*Capture event*/ );
+    }, true);
 
     $('[rel="popover"]').on('shown.bs.popover', function () {
         $('.coffee').addClass('coffee-hover');
